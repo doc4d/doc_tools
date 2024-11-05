e54d2362dc077cd2d3b2cc9292f8b0aa98781796
f4r3n <guillaume_k@hotmail.fr>
2024-11-05 21:08:41 +0000

    add more logs

--- opti_images/src/main.rs
+++ opti_images/src/main.rs
@@ -1,4 +1,4 @@
-use glob::glob;
+use glob::{glob, glob_with};
 use oxipng::{optimize, Options};
 use std::env;
 
@@ -6,19 +6,28 @@ fn main() -> Result<(), anyhow::Error> {
     let args: Vec<String> = env::args().collect();
     let directory = &args[1];
 
-    for entry in glob(directory)? {
-        let path = entry?.to_path_buf();
+    let glob_options = glob::MatchOptions {
+        case_sensitive: false,
+        require_literal_separator: false,
+        require_literal_leading_dot: false,
+    };
+    //../build/**/*.png
+    for entry in glob_with(directory, glob_options)? {
+        match entry {
+            Ok(path) => {
+                // Create default options for Oxipng
+                println!("{}", path.clone().display());
+                // Optimize the image
+                let in_file = oxipng::InFile::from(&path);
+                let out_file = oxipng::OutFile::from_path(path.clone());
+                let options = Options::default();
 
-        // Create default options for Oxipng
-        let options = Options::default();
-        println!("{}", path.clone().display());
-        // Optimize the image
-        let in_file = oxipng::InFile::from(&path);
-        let out_file = oxipng::OutFile::from_path(path.clone());
-
-        match optimize(&in_file, &out_file, &options) {
-            Ok(_) => println!("Optimization successful!"),
-            Err(err) => eprintln!("Optimization failed: {}", err),
+                match optimize(&in_file, &out_file, &options) {
+                    Ok(_) => println!("Optimization successful!"),
+                    Err(err) => println!("Optimization failed: {}", err),
+                }
+            }
+            Err(e) => println!("{:?}", e),
         }
     }
 
