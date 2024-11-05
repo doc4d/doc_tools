57aa29a8a3b2365746c44637107a68bba634401f
f4r3n <guillaume_k@hotmail.fr>
2024-11-05 16:11:52 +0000

    add tools

---
+++ opti_images/src/main.rs
@@ -0,0 +1,26 @@
+use glob::glob;
+use oxipng::{optimize, Options};
+use std::{env, path};
+
+fn main() -> Result<(), anyhow::Error> {
+    let args: Vec<String> = env::args().collect();
+    let directory = &args[1];
+
+    for entry in glob(format!("{}../../**/*.png", directory).as_str())? {
+        let path = entry?.to_path_buf();
+
+        // Create default options for Oxipng
+        let options = Options::default();
+        print!("{}", path.clone().display());
+        // Optimize the image
+        let in_file = oxipng::InFile::from(&path);
+        let out_file = oxipng::OutFile::from_path(path.clone());
+
+        match optimize(&in_file, &out_file, &options) {
+            Ok(_) => println!("Optimization successful!"),
+            Err(err) => eprintln!("Optimization failed: {}", err),
+        }
+    }
+
+    Ok(())
+}
